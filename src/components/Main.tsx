ba056f79209155754131aacdb55be500ff39eb35
upluke <lukeforadream@gmail.com>
2021-09-03 23:21:06 -0700

    set up a counter with useReducer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React,{useState,useReducer} from 'react'
+
+
+enum ActionType{
+    INCREMENT_COUNTER="INCREMENT_COUNTER",
+    DECREMENT_COUNTER="DECREMENT_COUNTER"
+}
+interface IReducer{
+    type:ActionType;
+    count:number    
+}
+
+interface ICounter{
+    count:number
+}
+const reducer:React.Reducer<ICounter, IReducer>=(state, action)=>{
+    return state
+}
+
 
 export const Main=()=>{
+    const [state, dispatch] = useReducer<React.Reducer<ICounter,IReducer>>(reducer, {count:0})
     return(
-        <div>Main</div>
+        <div>
+             
+        </div>
     )
 }
