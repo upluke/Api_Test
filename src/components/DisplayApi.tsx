7afdd3faa1126f568763031a057ed0a3e81d6d95
upluke <lukeforadream@gmail.com>
2021-09-06 00:33:13 -0700

    axios succeeded but fetch not

--- src/components/DisplayApi.tsx
+++ src/components/DisplayApi.tsx
@@ -1,36 +1,60 @@
 import React from 'react'
-
+import axios from 'axios'
 //https://randomuser.me/api
-async function fetchMyApi(){
-    let response= await fetch("https://randomuser.me/api")
-    let json= await response.json()
-    
-    return JSON.stringify(json,null,2)
-     
-     
- }
-  
-export const DisplayApi=()=>{
-    const [data, setData]=React.useState<any>('')
 
+// async function fetchMyApi(){
+//     let response= await fetch("https://randomuser.me/api")
+//     let json= await response.json()
+//     return json
+//  }
+
+function fetchMyApi(){
+    return axios.get('https://randomuser.me/api')
+  .then(({data})=> {
+    return data
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+   
+}
+
+const getFullUserName=(userInfo:any)=>{
+    const {name:{first, last}}=userInfo
+    return `${first} ${last}`
+}
+
+export const DisplayApi=()=>{
+    const [data, setData]=React.useState<any>("")
+    const [userInfos, setUserInfos]=React.useState<any>([])
 
    
 
-    React.useEffect(() => {
-        (async()=>{
-            let fetchData=await fetchMyApi()
-            setData(fetchData)
-        })()
-         
-        
-    }, [])
+    // React.useEffect(() => {
+    //     (async()=>{
+    //         let fetchData=await fetchMyApi()
+    //         setData(JSON.stringify(fetchData,null,2))
+    //         setUserInfos(data)   
+    //         console.log("results: ",userInfos)
+    //     })()
+    // }, [])
+     
+    React.useEffect(()=>{
+        fetchMyApi().then((data:any)=>{
+            setData(JSON.stringify(data,null,2))
+            setUserInfos(data.results)
+        })
+    },[])
+    console.log("data",userInfos)
 
-    console.log("data",data)
     return (
         <div>
-           {/* <div>{data.map((d:any)=>{
-              return (console.log(d))
-           })}</div>  */}
+            {userInfos.map((userInfo:any,idx:any)=>(
+                <div>
+                   <p>{getFullUserName(userInfo)}</p>
+               </div>
+           ))} 
            <pre>
                {data}
            </pre>
