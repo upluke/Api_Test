0637833e095559000e39956e7ee128da98e66974
upluke <lukeforadream@gmail.com>
2021-09-08 00:18:59 -0700

    TodoApp init

---
+++ src/components/TodoApp/TodoApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TodoAp:React.FC=() =>{
+    return (
+        <div>
+            TodoApp
+        </div>
+    )
+}
---
+++ src/components/TodoApp/TodoListItem.tsx
@@ -0,0 +1,18 @@
+import { typeParameterDeclaration } from '@babel/types'
+import React from 'react'
+
+interface TodoListItemProps{
+    todo:{
+        text:string;
+        complete:boolean;
+    }
+} 
+
+export const TodoListItem:React.FC<TodoListItemProps>=({todo})=>{
+    return (<li>
+            <label>
+                <input type="checkbox" checked={todo.complete} />
+                {todo.text}
+            </label>
+        </li>)
+}
