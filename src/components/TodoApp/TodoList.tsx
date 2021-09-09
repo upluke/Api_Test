d90e01a3013228e276955290265538bc04a24dc0
upluke <lukeforadream@gmail.com>
2021-09-09 12:26:44 -0700

    TodoList with its types

---
+++ src/components/TodoApp/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TodoListItem } from './TodoListItem'
+
+
+interface TodoListProps{
+    todos:Array<Todo>
+    toggleTodo:ToggleTodo
+}
+
+export const TodoList:React.FC<TodoListProps>=({todos,toggleTodo})=>{
+    return <ul>{todos.map((todo:Todo)=>{
+        return <TodoListItem todo={todo} toggleTodo={toggleTodo} />
+    })}</ul>
+}
