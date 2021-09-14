c2f8251fa811fb345386e4f2ec521162a9561934
upluke <lukeforadream@gmail.com>
2021-09-13 23:29:59 -0700

    added mosnter api

--- src/components/MonstersRolodex/Card.tsx
+++ src/components/MonstersRolodex/Card.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Card.css'
 
-export const Card:React.FC<any>=({name})=> {
+export const Card:React.FC<any>=({name,id})=> {
     return (
         <div className="card-container">
+            <img alt="monster" src={`https://robohash.org/${id}?set=set2&size=180x180`} />
             <h1>{name}</h1>
         </div>
     )
