42930cae25e8b880c0314e941905e63863a59b87
upluke <lukeforadream@gmail.com>
2021-09-14 22:54:09 -0700

    search input

--- src/components/MonstersRolodex/MonstersRolodex.tsx
+++ src/components/MonstersRolodex/MonstersRolodex.tsx
@@ -3,6 +3,7 @@ import { CardList } from './CardList'
 
 export const MonstersRolodex:React.FC=()=>{
     const [users, setUsers]=useState([])
+    const [searchField, setSearchField]=useState("")
     
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -10,11 +11,12 @@ export const MonstersRolodex:React.FC=()=>{
         .then(data=>setUsers(data))
     },[])
     
-    console.log("users: ",users)
-    
-     
+
+
     return(
         <div>
+            <input type="search" placeholder="search monsters" 
+            onChange={e=>{setSearchField(e.target.value)}} />
             <CardList users={users} />  
             
        
@@ -23,3 +25,68 @@ export const MonstersRolodex:React.FC=()=>{
         
     )
 } 
+
+
+    // Note:
+    // if just console.log(searchField) right after we set state , as we type, searchFiled key is always one
+    // letter behind, because setstate is an asynchronous funciton call.
+    // <input type="search" placeholder="search monsters" 
+    // onChange={e=>{setSearchField(e.target.value)
+    //     console.log(searchField)}} /> 
+    //  To solve this, 1) add a callback function to the second argument that we pass to setstate, it runs after
+    // setstate is finished.( works with old setState)
+    // 2) useEffect
+    // useEffect(() => {
+    // console.log('Do something after counter has changed', searchField);
+    // }, [searchField]);
+    // 3) functional update
+    // const [state, setState] = useState({name: "Michael"});
+    // const handleChangeName = () => {
+    //   setState({name: "Jack"})
+    //   setState(prevState => {
+    //     console.log(prevState.name);//"Jack"
+
+    //     //do something here
+
+    //     // return updated state
+    //     return prevState;
+    //   });
+    // }
+    // 4)useRef
+    // const [state, setState] = useState({name: "Michael"});
+    // const stateRef = useRef(state);
+    // stateRef.current  = state;
+    // const handleClick = () => {
+    //   setState({name: "Jack"});
+
+    //   setTimeout(() => {
+    //     //it refers to old state object
+    //     console.log(state.name);// "Michael";
+
+    //     //out of syntheticEvent and after batch update
+    //     console.log(stateRef.current.name);//"Jack"
+
+    //     //do something here
+    //   }, 0);
+    // }
+    // 5)custom hook with typescript
+    // import React, { useEffect, useRef, useState } from "react";
+
+    // export const useStateWithCallback = <T>(initialState: T): [state: T, setState: (updatedState: React.SetStateAction<T>, callback?: (updatedState: T) => void) => void] => {
+    // const [state, setState] = useState<T>(initialState);
+    // const callbackRef = useRef<(updated: T) => void>();
+
+    // const handleSetState = (updatedState: React.SetStateAction<T>, callback?: (updatedState: T) => void) => {
+    //     callbackRef.current = callback;
+    //     setState(updatedState);
+    // };
+
+    // useEffect(() => {
+    //     if (typeof callbackRef.current === "function") {
+    //         callbackRef.current(state);
+    //         callbackRef.current = undefined;
+    //     }
+    // }, [state]);
+
+    // return [state, handleSetState];
+    // }
